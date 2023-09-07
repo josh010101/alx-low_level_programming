7abbb40d0b73df205ca50a7e5b0b21ca06a720ab
josh010101 <gbeddiejosh@gmail.com>
2023-09-07 16:30:29 -0700

    C program that prints the size of various types on the computer it is compiled and run on.

--- 0x00-hello_world/6-sive.c
+++ 0x00-hello_world/6-sive.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - An entry point for the program
+ * main - Entrypoint for the program
  *
- * Description: Function that prints the size of different datatypes as 
- * stringed integers using the sizeof() function
+ * Description: the function prints the size of
+ * various datatypes as stringed integers using
+ * the sizeof() function
  * Return: 0
  */
-
 int main(void)
 {
-	printf("Size of a char: %1d byte(s)\n", sizeof(char));
-	printf("Size of an int: %1d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %1d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %1d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %1d byte(s)\n", sizeof(float));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
